53155b37f88699542a60c16611a55d95cf016196
RudyRMM1 <108768923+RudyRMM1@users.noreply.github.com>
2022-07-06 01:49:09 +0300

    Add files via upload

---
+++ java/1.js
@@ -0,0 +1,262 @@
+function openNav() {
+    document.getElementById("MySideBar").className="sidebar-show";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay").style.cursor="default";
+}
+function closeNav() {
+    document.getElementById("MySideBar").className="sidebar-hide";
+    document.getElementById("overlay").style.display ="none";
+
+}
+function fadeall(){
+    document.getElementById(1).className="fade";
+    document.getElementById(2).className="fade";
+    document.getElementById(3).className="fade";
+    document.getElementById(4).className="fade";
+    document.getElementById(5).className="fade";
+    document.getElementById(6).className="fade";
+    document.getElementById(7).className="fade";
+    document.getElementById(8).className="fade";
+    document.getElementById(9).className="fade";
+    document.getElementById(10).className="fade";
+    document.getElementById(11).className="fade";
+    document.getElementById(12).className="fade";
+    document.getElementById(13).className="fade";
+    document.getElementById(14).className="fade";
+    setTimeout(Home,500)
+    setTimeout(displayH,500)
+
+}
+function Home(){
+    document.getElementById(1).className="unfade";
+    document.getElementById(2).className="unfade";
+    document.getElementById(3).className="unfade";
+    document.getElementById(4).className="unfade";
+    document.getElementById(5).className="unfade";
+    document.getElementById(6).className="unfade";
+    document.getElementById(7).className="unfade";
+    document.getElementById(8).className="unfade";
+    document.getElementById(9).className="unfade";
+    document.getElementById(10).className="nufade";
+    document.getElementById(11).className="unfade";
+    document.getElementById(12).className="unfade";
+    document.getElementById(13).className="unfade";
+    document.getElementById(14).className="unfade";
+    
+}
+function displayH(){
+    document.getElementById(1).style.display = "block";
+    document.getElementById(2).style.display = "block";
+    document.getElementById(3).style.display = "block";
+    document.getElementById(4).style.display = "block";
+    document.getElementById(5).style.display = "block";
+    document.getElementById(6).style.display = "block";
+    document.getElementById(7).style.display = "block";
+    document.getElementById(8).style.display = "block";
+    document.getElementById(9).style.display = "block";
+    document.getElementById(10).style.display = "block";
+    document.getElementById(11).style.display = "block";
+    document.getElementById(12).style.display = "block";
+    document.getElementById(13).style.display = "block";
+    document.getElementById(14).style.display = "block";
+  
+}
+function Literature(){
+    document.getElementById(1).className="fade";
+    document.getElementById(2).className="unfade";
+    document.getElementById(3).className="unfade";
+    document.getElementById(4).className="fade";
+    document.getElementById(5).className="fade";
+    document.getElementById(6).className="fade";
+    document.getElementById(7).className="fade";
+    document.getElementById(8).className="fade";
+    document.getElementById(9).className="fade";
+    document.getElementById(10).className="fade";
+    document.getElementById(11).className="fade";
+    document.getElementById(12).className="fade";
+    document.getElementById(13).className="fade";
+    document.getElementById(14).className="fade";
+    setTimeout(displayL,600)
+}
+function displayL(){
+    document.getElementById(1).style.display = "none";
+    document.getElementById(2).style.display = "block";
+    document.getElementById(3).style.display = "block";
+    document.getElementById(4).style.display = "none";
+    document.getElementById(5).style.display = "none";
+    document.getElementById(6).style.display = "none";
+    document.getElementById(7).style.display = "none";
+    document.getElementById(8).style.display = "none";
+    document.getElementById(9).style.display = "none";
+    document.getElementById(10).style.display = "none";
+    document.getElementById(11).style.display = "none";
+    document.getElementById(12).style.display = "none";
+    document.getElementById(13).style.display = "none";
+    document.getElementById(14).style.display = "none";
+}
+function Mystery(){
+    document.getElementById(1).className="unfade";
+    document.getElementById(2).className="fade";
+    document.getElementById(3).className="fade";
+    document.getElementById(4).className="unfade";
+    document.getElementById(5).className="unfade";
+    document.getElementById(6).className="unfade";
+    document.getElementById(7).className="fade";
+    document.getElementById(8).className="fade";
+    document.getElementById(9).className="fade";
+    document.getElementById(10).className="fade";
+    document.getElementById(11).className="fade";
+    document.getElementById(12).className="fade";
+    document.getElementById(13).className="fade";
+    document.getElementById(14).className="fade";
+    setTimeout(displayM,600)
+}
+function displayM(){
+    document.getElementById(1).style.display = "block";
+    document.getElementById(2).style.display = "none";
+    document.getElementById(3).style.display = "none";
+    document.getElementById(4).style.display = "block";
+    document.getElementById(5).style.display = "block";
+    document.getElementById(6).style.display = "block";
+    document.getElementById(7).style.display = "none";
+    document.getElementById(8).style.display = "none";
+    document.getElementById(9).style.display = "none";
+    document.getElementById(10).style.display = "none";
+    document.getElementById(11).style.display = "none";
+    document.getElementById(12).style.display = "none";
+    document.getElementById(13).style.display = "none";
+    document.getElementById(14).style.display = "none";
+}
+function Drama(){
+    document.getElementById(1).className="fade";
+    document.getElementById(2).className="fade";
+    document.getElementById(3).className="fade";
+    document.getElementById(4).className="fade";
+    document.getElementById(5).className="fade";
+    document.getElementById(6).className="fade";
+    document.getElementById(7).className="unfade";
+    document.getElementById(8).className="unfade";
+    document.getElementById(9).className="fade";
+    document.getElementById(10).className="fade";
+    document.getElementById(11).className="fade";
+    document.getElementById(12).className="fade";
+    document.getElementById(13).className="fade";
+    document.getElementById(14).className="fade";
+    setTimeout(displayD,600)
+}
+function displayD(){
+    document.getElementById(1).style.display = "none";
+    document.getElementById(2).style.display = "none";
+    document.getElementById(3).style.display = "none";
+    document.getElementById(4).style.display = "none";
+    document.getElementById(5).style.display = "none";
+    document.getElementById(6).style.display = "none";
+    document.getElementById(7).style.display = "block";
+    document.getElementById(8).style.display = "block";
+    document.getElementById(9).style.display = "none";
+    document.getElementById(10).style.display = "none";
+    document.getElementById(11).style.display = "none";
+    document.getElementById(12).style.display = "none";
+    document.getElementById(13).style.display = "none";
+    document.getElementById(14).style.display = "none";
+}
+function Educational(){
+    document.getElementById(1).className="fade";
+    document.getElementById(2).className="fade";
+    document.getElementById(3).className="fade";
+    document.getElementById(4).className="fade";
+    document.getElementById(5).className="fade";
+    document.getElementById(6).className="fade";
+    document.getElementById(7).className="fade";
+    document.getElementById(8).className="fade";
+    document.getElementById(9).className="unfade";
+    document.getElementById(10).className="unfade";
+    document.getElementById(11).className="fade";
+    document.getElementById(12).className="fade";
+    document.getElementById(13).className="fade";
+    document.getElementById(14).className="fade";
+    setTimeout(displayE,600)
+}
+function displayE(){
+    document.getElementById(1).style.display = "none";
+    document.getElementById(2).style.display = "none";
+    document.getElementById(3).style.display = "none";
+    document.getElementById(4).style.display = "none";
+    document.getElementById(5).style.display = "none";
+    document.getElementById(6).style.display = "none";
+    document.getElementById(7).style.display = "none";
+    document.getElementById(8).style.display = "none";
+    document.getElementById(9).style.display = "block";
+    document.getElementById(10).style.display = "block";
+    document.getElementById(11).style.display = "none";
+    document.getElementById(12).style.display = "none";
+    document.getElementById(13).style.display = "none";
+    document.getElementById(14).style.display = "none";
+}
+function Biography(){
+    document.getElementById(1).className="fade";
+    document.getElementById(2).className="fade";
+    document.getElementById(3).className="fade";
+    document.getElementById(4).className="fade";
+    document.getElementById(5).className="fade";
+    document.getElementById(6).className="fade";
+    document.getElementById(7).className="fade";
+    document.getElementById(8).className="fade";
+    document.getElementById(9).className="fade";
+    document.getElementById(10).className="fade";
+    document.getElementById(11).className="unfade";
+    document.getElementById(12).className="unfade";
+    document.getElementById(13).className="fade";
+    document.getElementById(14).className="fade";
+    setTimeout(displayB,600)
+}
+function displayB(){
+    document.getElementById(1).style.display = "none";
+    document.getElementById(2).style.display = "none";
+    document.getElementById(3).style.display = "none";
+    document.getElementById(4).style.display = "none";
+    document.getElementById(5).style.display = "none";
+    document.getElementById(6).style.display = "none";
+    document.getElementById(7).style.display = "none";
+    document.getElementById(8).style.display = "none";
+    document.getElementById(9).style.display = "none";
+    document.getElementById(10).style.display = "none";
+    document.getElementById(11).style.display = "block";
+    document.getElementById(12).style.display = "block";
+    document.getElementById(13).style.display = "block";
+    document.getElementById(14).style.display = "none";
+}
+function Science(){
+    document.getElementById(1).className="fade";
+    document.getElementById(2).className="fade";
+    document.getElementById(3).className="fade";
+    document.getElementById(4).className="fade";
+    document.getElementById(5).className="fade";
+    document.getElementById(6).className="fade";
+    document.getElementById(7).className="fade";
+    document.getElementById(8).className="fade";
+    document.getElementById(9).className="fade";
+    document.getElementById(10).className="fade";
+    document.getElementById(11).className="fade";
+    document.getElementById(12).className="fade";
+    document.getElementById(13).className="unfade";
+    document.getElementById(14).className="unfade";
+    setTimeout(displayS,600)
+}
+function displayS(){
+    document.getElementById(1).style.display = "none";
+    document.getElementById(2).style.display = "none";
+    document.getElementById(3).style.display = "none";
+    document.getElementById(4).style.display = "none";
+    document.getElementById(5).style.display = "none";
+    document.getElementById(6).style.display = "none";
+    document.getElementById(7).style.display = "none";
+    document.getElementById(8).style.display = "none";
+    document.getElementById(9).style.display = "none";
+    document.getElementById(10).style.display = "none";
+    document.getElementById(11).style.display = "none";
+    document.getElementById(12).style.display = "none";
+    document.getElementById(13).style.display = "block";
+    document.getElementById(14).style.display = "block";
+}
+
